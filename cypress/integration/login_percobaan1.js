6c06ea08252cc498fc95f625db2272fc23c6e698
DhimasArbi <dhimas.arbi@gmail.com>
2021-11-24 09:35:08 +0700

    update cypress

--- cypress/integration/login_percobaan1.js
+++ cypress/integration/login_percobaan1.js
@@ -5,7 +5,4 @@ context('Cypress Demo-login', () => {
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
     })
-    it('Sort', ()=>{
-        cy.get('[data-test=product_sort_container]').select('za') // klik sort
-    })
 })
